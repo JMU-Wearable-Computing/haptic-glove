c4a74dc272857197a93b9a69a0c3555763ae28bb
Tyler <tylerwebster206@gmail.com>
2022-02-15 13:33:06 -0500

    Add Eagle files

--- Python/mapped_messaging.py
+++ Python/mapped_messaging.py
@@ -14,7 +14,7 @@ s.connect((TCP_IP, TCP_PORT))
 
 
 def make_message(vect):
-    return f'/{vect[0]}/{vect[1]}/{vect[2]}/{vect[3]}'
+    return f'/{vect[0]}/{vect[1]}/{vect[2]}/{vect[3]}\n'
 
 
 C = np.array([0,0,0]) #Current Pos
@@ -22,14 +22,15 @@ G = np.array([0,1,1]) # Goal Pos
 motors = np.array([np.array([0,0,1]), np.array([0,0,-1]), np.array([0,-1,0]), np.array([0,1,0])]) #array of motor positions
 
 for i in range(0,100):
-    G = np.array([0,random.uniform(-2,2),random.uniform(-2,2)]) # Goal Pos
+    G = np.array([0,random.uniform(-2,2),random.uniform(-2,2)]) # Goal Position
     
     intensity = find_intensity_array(C, G, motors)
     print(G, intensity)
     message = make_message(intensity)
 
     for i in range(0,10):
-        s.send(f'{message}\n'.encode('ascii'))
+        s.send(message.encode('ascii'))
     
     time.sleep(2)
 
+    
