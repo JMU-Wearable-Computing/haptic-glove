3ed7ea7792a785367b595a54078b60a1149b25c7
Wearable-Computer-Titus <jforsyth.jmu+labcomputer@gmail.com>
2023-09-26 17:01:54 -0400

    Change IP lookup to resolve multiple NICs issue

--- Python/glove.py
+++ Python/glove.py
@@ -24,7 +24,11 @@ class Glove():
         self.device_id = device_id
         self.verbose = verbose
         #Automatically find glove IP with device_id
-        self.TCP_IP = find_device_ip(self.device_id)
+        #self.TCP_IP = find_device_ip(self.device_id)
+
+        # remove automatic lookup to avoid error when multiple NICs are present
+        # IP is hard coded to 172.16.1.X based upon Apple AirPort router
+        self.TCP_IP = "172.16.1." + str(device_id)
         self.TCP_PORT = port
         #mode sets whether the vibrations tells use to move away or towards vibration
         self.mode = mode
@@ -170,7 +174,7 @@ class Glove():
 
 if __name__ == '__main__':
     #Define glove
-    glove = Glove(4, 8888, acceleration=True, verbose=False)
+    glove = Glove(4, 8888, acceleration=True, verbose=True)
     #Connect to glove
     glove.connect()
     #Setup keyboard listener
